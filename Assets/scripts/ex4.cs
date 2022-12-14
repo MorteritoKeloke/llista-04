f13ee07bcfec4b478c5ff6a2aa2e5bc9d1d4c064
MorteritoKeloke <zoroarkb@gmail.com>
2022-12-15 00:10:47 +0100

    f
    f

--- Assets/scripts/ex4.cs
+++ Assets/scripts/ex4.cs
@@ -6,13 +6,12 @@ public class ex4 : MonoBehaviour
 {
     public GameObject[] Prefabs; 
     private int Array;
-void Start()
+    void Start()
     {
         RanFigure()
          }
      private void RanFigure()
     {
         Array = Random.Range(0, Prefabs.Length); 
-        RanPos(), Prefabs[Array].transform.rotation;
     }
 }
